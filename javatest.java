95bf470866ff9f0dae464cdf2afd82f354c21464
bruceyoung <2371836990@qq.com>
2019-04-03 16:08:46 +0800

    增加了say2()方法

--- javatest.java
+++ javatest.java
@@ -1,9 +1,12 @@
-public class Demo{
+﻿public class Demo{
 	public void hello(){
 		System.out.println("Hello world!");
 	}
 	
 	public void say(){
 		System.out.println("你好，这是第一次测试！");		
+	}
+	public void say2(){
+		System.out.println("你好，这是第二次测试！");		
 	}
 }
